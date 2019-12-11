ef4c9eb7b2b18960505e0c65041360ae02148ca6
Kenneth NG <kwcng@hku.hk>
2019-12-11 02:29:23 +0000

    Add get current user
    Add header to show the current user and grahql to fetch

---
+++ src/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useMeQuery } from "./generated/graphql";
+
+interface Props {}
+
+export const Header: React.FC<Props> = () => {
+  const { data } = useMeQuery({ fetchPolicy: "network-only" });
+
+  return (
+    <header>
+      <div>
+        <Link to="/">home</Link>
+      </div>
+      <div>
+        <Link to="/register">register</Link>
+      </div>
+      <div>
+        <Link to="/login">login</Link>
+      </div>
+      <div>
+        <Link to="/bye">bye</Link>
+      </div>
+      {data && data.me ? <div>you are logged in as:{data.me.email}</div> : null}
+    </header>
+  );
+};
