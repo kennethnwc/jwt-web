88f9168d1e642b67cd48df71b8a75fb4df7afe7f
Kenneth NG <kwcng@hku.hk>
2019-12-11 02:14:40 +0000

    Comment on global accessToken
    Not a good practice to have a global variable

--- src/accessToken.ts
+++ src/accessToken.ts
@@ -1,4 +1,6 @@
-export let accessToken: string;
+// not a goood practice since the react page may rerender 
+// use some state management library to solve this
+let accessToken: string;
 
 export const setAccessToken = (s: string) => {
     accessToken = s
